c7e855f0dcbbe3d584ae9fde08430cf00e637b0c
leohlgctzby <18207221@qq.com>
2019-01-05 21:02:03 +0800

    下载依赖模块，引入antd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import { Button } from 'antd-mobile';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render(<Button type="primary">test</Button>, document.getElementById("root"));
