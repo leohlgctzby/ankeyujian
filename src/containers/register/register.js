3b1a23d3dd18ec4afb7d019991917bf4ea06a669
leohlgctzby <18207221@qq.com>
2019-01-06 15:52:38 +0800

    收集表单数据

--- src/containers/register/register.js
+++ src/containers/register/register.js
@@ -5,31 +5,55 @@ import Logo from '../../components/logo/logo';
 const ListItem = List.Item;
 
 export default class Register extends Component {
+  state = {
+    username: '',
+    password: '',
+    password2: '',
+    type: 'laoban',
+  }
+
+  //[name] 取变量的值
+  handleChange = (name, val) => {
+    this.setState({
+      [name]: val
+    })
+  }
+
+  register = () => {
+    console.log(this.state);
+  }
+
+  //跳转到login
+  toLogin = () => {
+    this.props.history.replace('/login')
+  }
+
   render() {
-    return (
+    const { type } = this.state;
+  return (
       <div>
         <NavBar>硅&nbsp;谷&nbsp;直&nbsp;聘</NavBar>
         <Logo/>
         <WingBlank>
           <List>
           <WhiteSpace/>
-            <InputItem >用户名:</InputItem> 
+            <InputItem onChange={val => {this.handleChange('username', val) }}>用户名:</InputItem> 
             <WhiteSpace/>
-            <InputItem type='password'>密&nbsp;&nbsp;&nbsp;码:</InputItem> 
+            <InputItem type='password' onChange={val => {this.handleChange('password', val) }}>密&nbsp;&nbsp;&nbsp;码:</InputItem> 
             <WhiteSpace/>
-            <InputItem type='password'>确认密码:</InputItem> 
+            <InputItem type='password' onChange={val => {this.handleChange('password2', val) }}>确认密码:</InputItem> 
             <WhiteSpace/>
               <ListItem>
                 <span>用户类型:</span>
                 &nbsp;&nbsp;&nbsp;
-                <Radio>老板</Radio>
+                <Radio checked={type==="laoban"} onChange={val => {this.handleChange('type', 'laoban')} }>老板</Radio>
                 &nbsp;&nbsp;&nbsp;
-                <Radio>大神</Radio>
+                <Radio checked={type==="dashen"} onChange={val => {this.handleChange('type', 'dashen')} }>大神</Radio>
               </ListItem>
               <WhiteSpace/>
-              <Button type='primary'> 注&nbsp;&nbsp;&nbsp;册</Button>
+              <Button type='primary' onClick={this.register}> 注&nbsp;&nbsp;&nbsp;册</Button>
               <WhiteSpace/>
-              <Button>已有账户</Button>
+              <Button onClick={this.toLogin}>已有账户</Button>
           </List>
         </WingBlank>
       </div>
