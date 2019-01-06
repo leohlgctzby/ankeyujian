ebda6f3e3cad15a27474ef890c9718dd66767587
leohlgctzby <18207221@qq.com>
2019-01-06 10:22:18 +0800

    引入路由

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Button } from 'antd-mobile';
+import React from "react";
+import ReactDom from "react-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Register from "./containers/register/register";
+import Login from "./containers/login/login";
+import Main from "./containers/main/main";
 
-ReactDom.render(<Button type="primary">test</Button>, document.getElementById("root"));
+ReactDom.render(
+  <HashRouter>
+    <Switch>
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
+      <Route component={Main} />
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
+);
