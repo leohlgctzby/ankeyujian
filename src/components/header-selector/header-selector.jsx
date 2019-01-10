73de9cd6f4464a4ca05a798336a7dedb720ed7f2
leohlgctzby <18207221@qq.com>
2019-01-10 16:49:36 +0800

    用户信息完善组件，收集数据

--- src/components/header-selector/header-selector.jsx
+++ src/components/header-selector/header-selector.jsx
@@ -2,8 +2,17 @@
 
 import React,{Component} from 'react';
 import {List, Grid} from 'antd-mobile'
+import PropTypes from 'prop-types'
 
 export default class HeadSelector extends Component{
+
+  static propTypes = {
+    setHeader: PropTypes.func.isRequired
+  }
+
+  state = {
+    icon: null //图片对象
+  }
 
   constructor(props) {
     super(props)
@@ -16,14 +25,27 @@ export default class HeadSelector extends Component{
     }
   }
 
+  // 解构el为{text, icon}
+  handleClick = ({text, icon}) => { 
+    this.setState({icon})
+    this.props.setHeader(text)
+  }
+
    render(){
      //头部界面
-     const listHeader = '请选择头部'
+    const { icon } = this.state
+
+     const listHeader = !icon ? '请选择头部' : (
+      <div>
+      已选择图像:<img src={icon}/>
+      </div>
+     )
        return (
 
             <List renderHeader={() => listHeader } >
               <Grid data={this.headerList} 
-                    columnNum={5}/>
+                    columnNum={5}
+                    onClick={this.handleClick} />
             </List>
 
        )
