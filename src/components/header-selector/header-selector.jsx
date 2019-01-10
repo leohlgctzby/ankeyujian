147726d5bd51554e5a9f383c1049fadfd8ddedc8
leohlgctzby <18207221@qq.com>
2019-01-10 11:17:51 +0800

    laoban-info静态组件

---
+++ src/components/header-selector/header-selector.jsx
@@ -0,0 +1,12 @@
+//选择用户头像的UI组件
+
+import React,{Component} from 'react';
+export default class HeadSelector extends Component{
+   render(){
+       return (
+           <div>
+           HeadSelector
+           </div>
+       )
+   }
+}
---
+++ src/containers/dashen-info/dashen-info.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux';
+
+class DashenInfo extends Component{
+   render(){
+       return (
+           <div>
+           DashenInfo
+           </div>
+       )
+   }
+}
+
+export default connect(
+  state => ({
+
+  }),
+  {}
+)(DashenInfo)
