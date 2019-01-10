e5b06d40905ebcf2b6f20bdfd00a3a5d769f49ae
leohlgctzby <18207221@qq.com>
2019-01-10 15:54:05 +0800

    laoban-info 头像部分

--- src/components/header-selector/header-selector.jsx
+++ src/components/header-selector/header-selector.jsx
@@ -1,12 +1,31 @@
 //选择用户头像的UI组件
 
 import React,{Component} from 'react';
+import {List, Grid} from 'antd-mobile'
+
 export default class HeadSelector extends Component{
+
+  constructor(props) {
+    super(props)
+    this.headerList = []
+    for (let i = 0; i < 20; i++) {
+      this.headerList.push({
+        text: '头像'+ (i+1), 
+        icon: require('./images/头像'+ (i+1) +'.png' )
+      })
+    }
+  }
+
    render(){
+     //头部界面
+     const listHeader = '请选择头部'
        return (
-           <div>
-           HeadSelector
-           </div>
+
+            <List renderHeader={() => listHeader } >
+              <Grid data={this.headerList} 
+                    columnNum={5}/>
+            </List>
+
        )
    }
 }
