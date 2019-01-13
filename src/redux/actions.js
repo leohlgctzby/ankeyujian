4c4c02577e442563d74a2ccffeb7acfc8a19594e
leohlgctzby <18207221@qq.com>
2019-01-13 18:08:05 +0800

    完善用户信息-ajax_redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,55 +1,74 @@
-import { reqRegister, reqLogin } from '../api'
-import { 
+import { reqRegister, reqLogin, reqUpdateUser } from "../api";
+import {
   AUTH_SUCCESS,
-  ERROR_MSG
-} from './action-types'
+  ERROR_MSG,
+  RECEIVE_USER,
+  RESET_USER
+} from "./action-types";
 
 //授权成功的同步action
-const authSuccess = (user) => ({type: AUTH_SUCCESS, data: user})
+const authSuccess = user => ({ type: AUTH_SUCCESS, data: user });
 //错误提示信息的同步action
-const errorMsg = (msg) => ({type: ERROR_MSG, data: msg})
+const errorMsg = msg => ({ type: ERROR_MSG, data: msg });
+//接收用户的同步action
+const receiveUser = user => ({ type: RECEIVE_USER, data: user });
+//重置用户的同步action
+const resetUser = msg => ({ type: RESET_USER, data: msg });
 
 //注册异步action
-export const register = (user) => {
-const {username, password, password2, type} = user
-//表单的前台检查
-if(!username) {
-  return errorMsg('用户名必须指定！')
-} else if(password!==password2) {
-  return errorMsg('2次密码要一致！')
-}
- 
-//表单数据合法，返回一个发ajax请求的异步action函数
-  return async dispatch => {
-    const response = await reqRegister({username, password, type})
-    const result = response.data
-    if(result.code===0) {
-      dispatch(authSuccess(result.data))
-    } else {
-      dispatch(errorMsg(result.msg))
-    } 
+export const register = user => {
+  const { username, password, password2, type } = user;
+  //表单的前台检查
+  if (!username) {
+    return errorMsg("用户名必须指定！");
+  } else if (password !== password2) {
+    return errorMsg("2次密码要一致！");
   }
-}
+
+  //表单数据合法，返回一个发ajax请求的异步action函数
+  return async dispatch => {
+    const response = await reqRegister({ username, password, type });
+    const result = response.data;
+    if (result.code === 0) {
+      dispatch(authSuccess(result.data));
+    } else {
+      dispatch(errorMsg(result.msg));
+    }
+  };
+};
 
 //登录异步action
-export const login = (user) => {
-
-  const {username, password} = user
+export const login = user => {
+  const { username, password } = user;
   //表单的前台检查
-  if(!username) {
-    return errorMsg('用户名必须指定！')
-  } else if(!password) {
-    return errorMsg('密码必须指定！')
+  if (!username) {
+    return errorMsg("用户名必须指定！");
+  } else if (!password) {
+    return errorMsg("密码必须指定！");
   }
 
   return async dispatch => {
-    const response = await reqLogin(user)
-    const result = response.data
-    if(result.code===0) {
-      dispatch(authSuccess(result.data))
+    const response = await reqLogin(user);
+    const result = response.data;
+    if (result.code === 0) {
+      dispatch(authSuccess(result.data));
     } else {
-      dispatch(errorMsg(result.msg))
+      dispatch(errorMsg(result.msg));
     }
+  };
+};
 
-  }
-}
+//更新用户异步action
+export const updateUser = user => {
+  return async dispatch => {
+    const response = await reqUpdateUser(user);
+    const result = response.data;
+    if (result.code === 0) {
+      //更新成功：data
+      dispatch(receiveUser(result.data));
+    } else {
+      //更新失败：msg
+      dispatch(resetUser(result.msg));
+    }
+  };
+};
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,29 +1,35 @@
-import { combineReducers } from 'redux';
-import { 
+import { combineReducers } from "redux";
+import {
   AUTH_SUCCESS,
-  ERROR_MSG
-} from './action-types'
-import { getRedirectTo } from '../utils'
+  ERROR_MSG,
+  RECEIVE_USER,
+  RESET_USER
+} from "./action-types";
+import { getRedirectTo } from "../utils";
 
 const initUser = {
-  username: '', //用户名
-  type: '', //用户类型
-  msg: '', // 错误提示信息
-  redirectTo: ''//需要自动重定向的路由路径
-}
+  username: "", //用户名
+  type: "", //用户类型
+  msg: "", // 错误提示信息
+  redirectTo: "" //需要自动重定向的路由路径
+};
 
-function user(state=initUser, action) {
+function user(state = initUser, action) {
   switch (action.type) {
     case AUTH_SUCCESS:
-      const {type, header} = action.data
-      return {...action.data, redirectTo: getRedirectTo(type, header)} 
+      const { type, header } = action.data;
+      return { ...action.data, redirectTo: getRedirectTo(type, header) };
     case ERROR_MSG:
-      return {...state, msg: action.data} 
+      return { ...state, msg: action.data };
+    case RECEIVE_USER:
+      return action.data;
+    case RESET_USER:
+      return { ...initUser, msg: action.data };
     default:
-      return state
+      return state;
   }
 }
 
 export default combineReducers({
   user
-})
+});
