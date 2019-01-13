0a9bf9ccc5af909cbea04106d43c475898314a13
leohlgctzby <18207221@qq.com>
2019-01-13 22:37:43 +0800

    检查用户是否登录，如果没有，自动重定向到登录界面

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -1,11 +1,19 @@
 import React,{Component} from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux'
 
 import LaobanInfo from '../laoban-info/laoban-info'
 import DashenInfo from '../dashen-info/dashen-info'
 
-export default class Main extends Component{
+class Main extends Component{
    render(){
+     //检查用户是否登录，如果没有，自动重定向到登录界面
+     //在主路由做，相当于下面的所有路由都有了
+     const {user} = this.props
+      if(!user._id) {
+        return <Redirect to='/login'/>
+      }
+
        return (
            <div>
            <Switch>
@@ -17,3 +25,7 @@ export default class Main extends Component{
        )
    }
 }
+
+export default connect(
+  state => ({user: state.user})
+)(Main)
