2e488dd86b369423df20d8143af70d0c3b3ce225
leohlgctzby <18207221@qq.com>
2019-01-14 15:37:20 +0800

    主界面流程编码2

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import Cookie from "js-cookie"; //可以操作前端cookie的对象set() remove（)
+import Cookies from "js-cookie"; //可以操作前端cookie的对象set() remove（)
 
 import LaobanInfo from "../laoban-info/laoban-info";
 import DashenInfo from "../dashen-info/dashen-info";
@@ -9,14 +9,13 @@ import DashenInfo from "../dashen-info/dashen-info";
 import { getRedirectTo } from "../../utils";
 
 class Main extends Component {
-
   componentDidMount() {
     //之前登录过（cookie中有userid），但是现在还没登录（redux管理的user中没有_id），发请求获取对应的user
     const userid = Cookies.get("userid");
     const { _id } = this.props.user;
-    if(userid && !_id) {
+    if (userid && !_id) {
       //发送异步请求，获取user
-      console.log('发送ajax请求获取user')
+      console.log("发送ajax请求获取user");
     }
   }
 
@@ -30,6 +29,7 @@ class Main extends Component {
     //如果有，读取redux中的user的状态
     const { user } = this.props;
     //如果user没有_id，返回一个null（不做任何显示）
+    debugger
     if (!user._id) {
       return null;
     } else {
@@ -69,6 +69,6 @@ export default connect(state => ({ user: state.user }))(Main);
 //      之前登录过（cookie中有userid），但是现在还没登录（redux管理的user中没有_id），发请求获取对应的user
 //   2.render（）
 //    1） 如果cookie中没有userid，直接重定向到login界面
-//    2）判断redux管理的user中是否有_id, 如果没有，暂时不做任何显示
+//    2） 判断redux管理的user中是否有_id, 如果没有，暂时不做任何显示
 //    3） 如果有，说明当前已经登录，显示对应的界面
 //    4）如果请求的是根路径，根据user的type和header，计算出重定向的路径，自动重定向
