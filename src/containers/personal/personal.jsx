e80534c397e13e1fb3e0eba194af2f5d7cdde408
leohlgctzby <18207221@qq.com>
2019-01-15 15:04:21 +0800

    静态个人中心界面

--- src/containers/personal/personal.jsx
+++ src/containers/personal/personal.jsx
@@ -1,17 +1,37 @@
-import React,{Component} from 'react';
-import {connect} from 'react-redux'
-
- class Personal extends Component{
-   render(){
-       return (
-           <div>
-               Personal
-           </div>
-       )
-   }
+/*
+用户个人中心路由组件
+*/
+import React from "react";
+import { Result, List, WhiteSpace, Button } from "antd-mobile";
+const Item = List.Item;
+const Brief = Item.Brief;
+export default class Personal extends React.Component {
+  render() {
+    return (
+      <div>
+        <Result
+          img={
+            <img
+              src={require(`../../assets/images/头像1.png`)}
+              style={{ width: 50 }}
+              alt="header"
+            />
+          }
+          title="张三"
+          message="IBM"
+        />
+        <List renderHeader={() => "相关信息"}>
+          <Item multipleLine>
+            <Brief>职位: 前端工程师</Brief>
+            <Brief>简介: React/Vue/jQuery</Brief>
+            <Brief>薪资: 20k</Brief>
+          </Item>
+        </List>
+        <WhiteSpace />
+        <List>
+          <Button type="warning">退出登录</Button>
+        </List>
+      </div>
+    );
+  }
 }
-
-export default connect(
-  state => ({}),
-  {}
-)(Personal)
