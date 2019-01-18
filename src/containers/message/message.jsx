426d211fac5bf1209ee04c9078ead43c42192a3b
leohlgctzby <18207221@qq.com>
2019-01-18 22:20:05 +0800

    对话消息列表组件—静态

--- src/containers/message/message.jsx
+++ src/containers/message/message.jsx
@@ -1,17 +1,36 @@
-import React,{Component} from 'react';
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { List, Badge } from "antd-mobile";
 
- class Message extends Component{
-   render(){
-       return (
-           <div>
-               Message
-           </div>
-       )
-   }
+const Item = List.Item;
+const Brief = Item.Brief;
+
+class Message extends Component {
+  render() {
+    return (
+      <List>
+        <Item
+          extra={<Badge text={3} />}
+          thumb={require(`../../assets/images/头像1.png`)}
+          arrow="horizontal"
+        >
+          你好
+          <Brief>nr1</Brief>
+        </Item>
+        <Item
+          extra={<Badge text={0} />}
+          thumb={require(`../../assets/images/头像2.png`)}
+          arrow="horizontal"
+        >
+          你好2
+          <Brief>nr2</Brief>
+        </Item>
+      </List>
+    );
+  }
 }
 
 export default connect(
   state => ({}),
   {}
-)(Message)
+)(Message);
