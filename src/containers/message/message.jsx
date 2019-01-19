cee8c333ad9a316f6ae9013fac3b6a08d3cb6b76
leohlgctzby <18207221@qq.com>
2019-01-19 16:31:26 +0800

    显示聊天组件的未读消息数量

--- src/containers/message/message.jsx
+++ src/containers/message/message.jsx
@@ -9,23 +9,34 @@ const Brief = Item.Brief;
 //1.找出每个聊天的lastMsg，并用一个对象容器来保存{chat_id: lastMsg}
 //2.得到所有lastMsg的数组，
 //3.对数组进行排序（按时间create_time降序）
-function getLastMsgs(chatMsgs) {
+function getLastMsgs(chatMsgs, userid) {
   //1.找出每个聊天的lastMsg，并用一个对象容器来保存{chat_id: lastMsg}
   const lastMsgObjs = {};
   chatMsgs.forEach(msg => {
+
+    //对msg进行个体的统计,别人发给我，并且未读才加1
+    if(msg.to===userid && !msg.read) {
+      msg.unReadCount = 1
+    } else {
+      msg.unReadCount = 0
+    }
+
     //得到msg的聊天标识id
     const chatId = msg.chat_id;
     //获取已经保存的当前组的lastMsg
     const lastMsg = lastMsgObjs[chatId];
-    if (!lastMsg) {
-      //当前msg就是所在组的lastMsg
+    //没有
+    if (!lastMsg) {//当前msg就是所在组的lastMsg
       lastMsgObjs[chatId] = msg;
-    } else {
-      //有
+    } else {//有
+      //累加unReadCount = 已经统计的 + 当前的msg的
+      const unReadCount = lastMsg.unReadCount + msg.unReadCount
       //如果msg比lastMsg晚，就将msg保存为lastMsg
       if (msg.create_time > lastMsg.create_time) {
         lastMsgObjs[chatId] = msg;
       }
+      //将unReadCount保存到最新的lastMsg上
+      lastMsgObjs[chatId].unReadCount = unReadCount
     }
   });
   //2.得到所有lastMsg的数组
@@ -43,7 +54,7 @@ class Message extends Component {
     const { user } = this.props;
     const { users, chatMsgs } = this.props.chat;
 
-    const lastMsgs = getLastMsgs(chatMsgs);
+    const lastMsgs = getLastMsgs(chatMsgs, user._id);
 
     return (
       <List style={{ marginTop: 50, marginBottom: 50 }}>
@@ -54,7 +65,7 @@ class Message extends Component {
           return (
             <Item
               key={msg._id}
-              extra={<Badge text={3} />}
+              extra={<Badge text={msg.unReadCount} />}
               thumb={
                 targetUser.header
                   ? require(`../../assets/images/${targetUser.header}.png`)
